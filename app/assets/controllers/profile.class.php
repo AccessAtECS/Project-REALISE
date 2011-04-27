6b695719dbea4eec522290e87f37715a159249b3
Sebastian Skuse <seb@skuse.net>
2011-04-27 16:39:45 +0100

    Skeleton for profile page

---
+++ app/assets/controllers/profile.class.php
@@ -0,0 +1,35 @@
+<?php
+
+class controller_profile extends controller {
+
+	private $m_user;
+
+	public function renderViewport() {
+		$this->m_user = $this->objects("user");
+		
+		// Select the tab	
+		util::selectTab($this->superview(), "home");
+
+		util::userBox($this->m_user, $this->superView());
+				
+		$this->superview()->replace("sideContent", util::displayNewInnovators());
+
+		$this->bindDefault('displayProfile');
+	}
+	
+	protected function displayProfile(){
+		$this->setViewport(new view("profile"));
+		
+		$this->viewport()->replaceAll(array(
+			"profileImage" => $this->m_user->getPicture(),
+			"tagline" => $this->m_user->getTagline(),
+			"email" => $this->m_user->getEmail()
+		));
+
+	}
+
+
+
+}
+
+?>
