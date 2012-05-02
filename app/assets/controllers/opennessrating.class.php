f017aa51c48fdb997d31488e2162c83222276f62
Magnus P White <mpw@ecs.soton.ac.uk>
2012-05-02 14:19:35 +0100

    Added questions

--- app/assets/controllers/opennessrating.class.php
+++ app/assets/controllers/opennessrating.class.php
@@ -37,31 +37,43 @@ class controller_opennessrating extends controller {
 	
 	protected function opennessInfo(){
 		$this->pageName = "- Openness Rating - Project Information";
-			
 		$this->setViewport(new view("openness-info"));
-		$questions = $this->getQuestions("info");
-		
-		$q = new view();
-		
-		foreach($questions[0] as $question){
-		
-			$template = new view('frag.opennessQuestionText');
-			$template->replace("question", $question['question']);
-			$template->replace("sub_question", $question['sub_question']);
-			$template->replace("section", $question['section']);
-			$template->replace("question_id", $question['id']);
-
-			$q->append($template->get());
-		}
-
-		$this->viewport()->replace("questions", $q);
+		$this->createQuestions("info");
 	}
 	
 	protected function opennessLegal(){
 		$this->pageName = "- Openness Rating - Legal";
-		
 		$this->setViewport(new view("openness-legal"));
-		$questions = $this->getQuestions("legal");
+		$this->createQuestions("legal");
+	}	
+	
+	protected function opennessStandards(){
+		$this->pageName = "- Openness Rating - Data Formats and Standards";
+		$this->setViewport(new view("openness-standards"));
+		$this->createQuestions("standards");
+	}	
+	
+	protected function opennessKnowledge(){
+		$this->pageName = "- Openness Rating - Knowledge";
+		$this->setViewport(new view("openness-knowledge"));
+		$this->createQuestions("knowledge");
+	}
+	
+	protected function opennessGovernance(){
+		$this->pageName = "- Openness Rating - Governance";
+		$this->setViewport(new view("openness-governance"));
+		$this->createQuestions("governance");
+	}
+	
+	protected function opennessMarket(){
+		$this->pageName = "- Openness Rating - Market";
+		$this->setViewport(new view("openness-market"));
+		$this->createQuestions("market");
+	}	
+	
+	protected function createQuestions($section){
+	
+		$questions = $this->getQuestions($section);
 		
 		$q = new view();
 			
@@ -78,84 +90,100 @@ class controller_opennessrating extends controller {
 
 					$answers = $this->getAnswers($question['id']);
 					$a = new view();
-					
+				
 					foreach($answers[0] as $answer){
-
 						$answerFrag = new view('frag.opennessAnswerDrop');
 						$answerFrag->replace("answer", $answer['answer']);
 						$answerFrag->replace("answer_id", $answer['id']);
 						$a->append($answerFrag->get());
-
-					}
-					
-					$answerFrag->replace("options", $a);
-					$answerFrag->reset();
+					}					
+					$template->replace("options", $a);
 					
 				break;
 				
 				case 'text':
-					$template = new view('frag.opennessQuestionText');
+
+					foreach($questions[0] as $question){
+					
+						$template = new view('frag.opennessQuestionText');
+						$template->replace("question", $question['question']);
+						$template->replace("sub_question", $question['sub_question']);
+						$template->replace("section", $question['section']);
+						$template->replace("question_id", $question['id']);
+
+						$q->append($template->get());
+					}
+
+					$this->viewport()->replace("questions", $q);
+				break;
+				
+				case 'multi-select':
+					$template = new view('frag.opennessQuestionMultiSelect');
 					$template->replace("question", $question['question']);
 					$template->replace("sub_question", $question['sub_question']);
 					$template->replace("section", $question['section']);
 					$template->replace("question_id", $question['id']);
-				break;
-				
-				case 'multi-select':
 
+					$answers = $this->getAnswers($question['id']);
+					$a = new view();
+				
+					foreach($answers[0] as $answer){
+						$answerFrag = new view('frag.opennessAnswerMultiSelect');
+						$answerFrag->replace("answer", $answer['answer']);
+						$answerFrag->replace("answer_id", $answer['id']);
+						$answerFrag->replace("section", $question['section']);
+						$a->append($answerFrag->get());
+					}					
+					$template->replace("options", $a);
+					
 				break;
 				
 				case 'scale':
+					$i = 0;
+					
+					$template = new view('frag.opennessQuestionScale');
+					$template->replace("question", $question['question']);
+					$template->replace("sub_question", $question['sub_question']);
+					$template->replace("question_id", $question['id']);
 
+					$answers = $this->getAnswers($question['id']);
+					$a = new view();
+				
+					foreach($answers[0] as $answer){
+						$answerFrag = new view('frag.opennessAnswerScale');
+						$answerFrag->replace("answer", $answer['answer']);
+						$answerFrag->replace("answer_id", $answer['id']);
+						$answerFrag->replace("section", $question['section']);
+						$answerFrag->replace("number", $i);
+						$template->replace("answer_id",$answer['id']);
+						$template->replace("answer".$i,$i);
+						$a->append($answerFrag->get());
+						
+						$i++;
+					}
+					$template->replace("answers", $a);
+					$i = 0;
 				break;
 				
 				default:
-					echo "";
+					echo "default question";
 			}
 			
 			$q->append($template->get());
 		}
 
 		$this->viewport()->replace("questions", $q);		
-	}	
-	
-	protected function opennessStandards(){
-		$this->pageName = "- Openness Rating - Data Formats and Standards";
-		
-		$this->setViewport(new view("openness-standards"));
-	}	
-	
-	protected function opennessKnowledge(){
-		$this->pageName = "- Openness Rating - Knowledge";
-		
-		$this->setViewport(new view("openness-knowledge"));
 	}
-	
-	protected function opennessGovernance(){
-		$this->pageName = "- Openness Rating - Governance";
-		
-		$this->setViewport(new view("openness-governance"));
-	}
-	
-	protected function opennessMarket(){
-		$this->pageName = "- Openness Rating - Market";
-		
-		$this->setViewport(new view("openness-market"));
-	}	
 	
 	protected function getQuestions($section){
-		
 		$this->db = db::singleton();
 		$results = $this->db->select(array("*"), "open_question", array(array("", "section", "=", $section)))->run();
-		
 		return $results;
 	}
 
 	protected function getAnswers($question_id){
-		
 		$this->db = db::singleton();
 		$results = $this->db->select(array("*"), "open_answer", array(array("", "open_question_id", "=", $question_id)))->run();
-		
 		return $results;
 	}		
 	
