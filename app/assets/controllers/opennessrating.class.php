08db0a8d325b3a431a3e3ed90ac2b93d3e5b9321
Magnus P White <mpw@ecs.soton.ac.uk>
2012-05-03 03:50:48 +0100

    More updates

--- app/assets/controllers/opennessrating.class.php
+++ app/assets/controllers/opennessrating.class.php
@@ -47,7 +47,8 @@ class controller_opennessrating extends controller {
 	
 		$this->pageName = "- Openness Rating - Project Information";
 		$this->setViewport(new view("openness-info"));
-		$this->createQuestions("info");
+		$this->viewport()->replace("project_id", $id);	
+		$this->createQuestions($id, "info");
 	}
 	
 	protected function opennessLegal(){
@@ -56,7 +57,8 @@ class controller_opennessrating extends controller {
 	
 		$this->pageName = "- Openness Rating - Legal";
 		$this->setViewport(new view("openness-legal"));
-		$this->createQuestions("legal");
+		$this->viewport()->replace("project_id", $id);
+		$this->createQuestions($id, "legal");
 	}	
 	
 	protected function opennessStandards(){
@@ -65,7 +67,8 @@ class controller_opennessrating extends controller {
 	
 		$this->pageName = "- Openness Rating - Data Formats and Standards";
 		$this->setViewport(new view("openness-standards"));
-		$this->createQuestions("standards");
+		$this->viewport()->replace("project_id", $id);
+		$this->createQuestions($id, "standards");
 	}	
 	
 	protected function opennessKnowledge(){
@@ -74,7 +77,8 @@ class controller_opennessrating extends controller {
 	
 		$this->pageName = "- Openness Rating - Knowledge";
 		$this->setViewport(new view("openness-knowledge"));
-		$this->createQuestions("knowledge");
+		$this->viewport()->replace("project_id", $id);
+		$this->createQuestions($id, "knowledge");
 	}
 	
 	protected function opennessGovernance(){
@@ -83,7 +87,8 @@ class controller_opennessrating extends controller {
 	
 		$this->pageName = "- Openness Rating - Governance";
 		$this->setViewport(new view("openness-governance"));
-		$this->createQuestions("governance");
+		$this->viewport()->replace("project_id", $id);
+		$this->createQuestions($id, "governance");
 	}
 	
 	protected function opennessMarket(){
@@ -92,12 +97,30 @@ class controller_opennessrating extends controller {
 	
 		$this->pageName = "- Openness Rating - Market";
 		$this->setViewport(new view("openness-market"));
-		$this->createQuestions("market");
+		$this->viewport()->replace("project_id", $id);
+		$this->createQuestions($id, "market");
 	}
 	
 	protected function infoProcess(){
-		echo("info process");
-		exit;
+		
+		$project_id = $_POST['project_id'];
+		$q = $this->getQuestionId("info");
+		$i = 1;
+		
+		foreach($q[0] as $n){
+			
+			$answer_id = $i;
+			$value = $_POST[$i];
+			//does answer for this project exist
+			
+			//if yes delete it first
+			
+			//if no or deleted - create new answer record
+			$this->createAnswer($answer_id, $project_id, $value);	
+			$i++;
+		}
+
+		$this->redirect("/opennessrating/legal/?id=".$project_id);
 	}
 
 	protected function legalProcess(){
@@ -125,14 +148,13 @@ class controller_opennessrating extends controller {
 		exit;
 	}
 	
-	protected function createQuestions($section){
+	protected function createQuestions($project_id, $section){
 	
 		$questions = $this->getQuestions($section);
 		
 		$q = new view();
-
-		try{		
 		
+		try{
 			foreach($questions[0] as $question){	
 			
 				switch ($question['type']){
@@ -160,12 +182,10 @@ class controller_opennessrating extends controller {
 							$answerFrag->replace("answer_id", $answer['id']);
 							$a->append($answerFrag->get());
 						}					
-						$template->replace("options", $a);
-						
+						$template->replace("options", $a);						
 					break;
 					
 					case 'text':
-
 						foreach($questions[0] as $question){
 						
 							$template = new view('frag.opennessQuestionText');
@@ -174,6 +194,14 @@ class controller_opennessrating extends controller {
 							$template->replace("section", $question['section']);
 							$template->replace("question_id", $question['id']);
 
+							$value = $this->viewAnswer($project_id, $question['id']);
+							
+							if(empty($value)){
+								$template->replace("value", "");
+							}
+							else {
+								$template->replace("value", $value);
+							}							
 							$q->append($template->get());
 						}
 
@@ -239,26 +267,43 @@ class controller_opennessrating extends controller {
 			
 		} catch(Exception $e){
 			echo "Unable to render openness rating question list. Please try again later.";
-		}
-		
+			exit;
+		}		
 	}
 	
 	protected function getQuestions($section){
 		$this->db = db::singleton();
-		$results = $this->db->select(array("*"), "open_question", array(array("", "section", "=", $section)))->run();
-		return $results;
+		$result = $this->db->select(array("*"), "open_question", array(array("", "section", "=", $section)))->run();
+		return $result;
 	}
 
 	protected function getAnswers($question_id){
 		$this->db = db::singleton();
-		$results = $this->db->select(array("*"), "open_answer", array(array("", "open_question_id", "=", $question_id)))->run();
-		return $results;
+		$result = $this->db->select(array("*"), "open_answer", array(array("", "open_question_id", "=", $question_id)))->run();
+		return $result;
 	}		
 	
 	protected function getQuestionId($section){
 		$this->db = db::singleton();
-		$results = $this->db->select(array("id"), "open_question", array(array("", "section", "=", $section)))->run();
-		return $results;
+		$result = $this->db->select(array("id"), "open_question", array(array("", "section", "=", $section)))->run();
+		return $result;
+	}
+	
+	protected function createAnswer($answer_id, $project_id, $value = NULL){
+		$this->db = db::singleton();
+		
+		try {
+			$result = $this->db->insert(array("answer_id" => $answer_id, "project_id" => $project_id, "value" => $value), "open_project_has_answer")->run();
+		} catch (Exception $e){
+			echo "error";
+			exit;
+		}
+	}
+	
+	protected function viewAnswer($project_id, $question_id){
+		$this->db = db::singleton();
+		$result = $this->db->select(array("value"), "open_project_has_answer", array(array("", "project_id", "=", $project_id)), "AND answer_id = ".$question_id)->run();		
+		return $result[0][0]['value'];
 	}
 	
 }
